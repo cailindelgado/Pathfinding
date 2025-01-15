d187ff2041f29fe62fd1c4c79d58846be6e46978
Cailin Delgado <c.delgado@uqconnect.edu.au>
2025-01-15 13:41:16 +1000

    stuck on assigning to struct stuff

---
+++ libdarray.c
@@ -0,0 +1,12 @@
+#include "libdarray.h"
+#
+
+darray *create_array(size_t initialCapacity) {
+
+    darray *arr = NULL;
+    // array = (darray *)
+    arr = (darray *) safe_malloc(sizeof (int));
+    assert (arr != NULL);
+    arr -> array
+    return (array);
+}
